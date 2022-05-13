002f324d8a1ce2c1a132299da8c86d6c9f5ced53
leeshy-tech <1335316512@qq.com>
2022-05-13 21:55:12 +0800

    调参

--- hyp.py
+++ hyp.py
@@ -1,12 +1,12 @@
 # hyp.py
 hyp = {
-    'embed_path': 'sgns.wiki.bigram', # 记得修改位置
+    'embed_path': '..\sgns.wiki.bigram', # 记得修改位置
     'data_path': 'data',
-    'batch_size': 128,
+    'batch_size': 2048,
     'nb_filters': 128,
     'dropout_rate': 0.3,
     'embed_size': 300,
-    'learning_rate': 0.05,
+    'learning_rate': 0.04,
     'epoches': 60,
-    'save_model_name': "checkpoint_textCNN.pt"
+    'save_model_name': "..\checkpoint_textCNN.pt"
 }
